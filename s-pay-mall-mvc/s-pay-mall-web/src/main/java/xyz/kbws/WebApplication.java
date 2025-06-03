467777c410465bbc6b2b62b9786c1593e8a47c03
kbws13 <hsy040506@163.com>
2025-06-03 23:39:37 +0800

    feat(s-pay-mall): 添加微信公众号对接功能
    - 新增微信公众号验证和消息处理相关代码
    - 添加日志配置和异步日志处理
    - 实现基础的微信消息收发功能
    - 添加 XML 和签名处理工具类

---
+++ s-pay-mall-mvc/s-pay-mall-web/src/main/java/xyz/kbws/WebApplication.java
@@ -0,0 +1,16 @@
+package xyz.kbws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kbws
+ * @date 2025/6/3
+ * @description:
+ */
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
