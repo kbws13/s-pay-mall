091a48e2328f931027ed5087f5d9fe0f44ec4d50
kbws13 <hsy040506@163.com>
2025-06-03 22:08:11 +0800

    feat(payment): 添加支付宝支付功能 - 新增 AliPayController 处理支付宝回调通知 - 添加支付宝支付相关的配置和密钥 - 实现支付宝支付页面和异步通知接口 - 配置日志记录和异步日志记录 - 更新项目结构，添加支付宝 SDK 依赖

---
+++ s-pay-mall-alipay/src/main/java/xyz/kbws/alipay/Application.java
@@ -0,0 +1,16 @@
+package xyz.kbws.alipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kbws
+ * @date 2025/6/2
+ * @description:
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
